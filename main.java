d4cc664099e38801cacac4db32433d4c1fdbfd5f
Hardik <hardik11dhiman@gmail.com>
2022-12-12 11:46:47 +0530

    Second commit

--- main.java
+++ main.java
@@ -1,22 +1,34 @@
 import javax.swing.*;
+import java.awt.FlowLayout;
 
-public class main
+
+class main
 {
-    //Hello world using java Swing
     public static void main(String[] args)
     {
-        Test object= new Test();
+        Test object = new Test();
     }
 
 }
+
 class Test extends JFrame
 {
-    //Using the constructor of JFrame
-    public Test()
+    public Test()                                        //Default constructor overriding
     {
-        JLabel l = new JLabel("Hello World");
+        JTextField t1= new JTextField(20);
+        JTextField t2= new JTextField(20);
+        JButton b1 = new JButton("=");
+        JLabel l= new JLabel("Result");        //Creating an object f
+
+        add(t1);
+        add(t2);
+        add(b1);
         add(l);
+
+
+        setLayout(new FlowLayout());                                  //This is for setting the layout so the objects don`t get overrided
         setVisible(true);
-        setSize(400,400);
+        setSize(600,600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);    //Terminates the process when close button is pressed. We can also write value 3 in brackets as Exit on Close gives the value 3.
     }
 }
